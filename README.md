84d2869b9dd63b34c24b22643c263125dda748aa
adrian <adriannovella99@gmail.com>
2023-10-21 12:30:20 +0200

    se reeescribe el codigo, ya que la api no se estaba interpretando de forma correcta.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Registro precios Mercadona
 
-Intento de crear un histórico de los precios de Mercadona, la idea es que estos datos sirvan para calcular métricas de variación de precios.
+Intento de crear un histórico de los precios de Mercadona.
+La idea es que estos datos sirvan para calcular métricas reales de variación de precios en España, basándonos directamente en uno de los supermercados más populares.
 
 No se si los datos de la API son fiables o se actualizan a menudo.
-El archivo data.json se irá actualizando por medio de github actions, a priori semanalmente, viendo la historia de git se podrán ver las diferencias.
+El archivo data.json se irá actualizando por medio de github actions, a priori semanalmente, viendo la historia de git se podrán ver las diferencias, si no se actualiza es porque no a habido cambios.
 
-Mas adelante si los datos resultan de utilidad se actualizará con alguna herramienta de análisis.
+Mas adelante si los datos resultan de utilidad se actualizará con alguna herramienta de análisis. Suponemos que los datos que nos da el INE son correctos, dentro de la parte subjetiva que tiene el IPC claro, pero nunca está de más revisar los datos oficiales.
+
+En el archivo [.excalidraw](https://excalidraw.com/) esta recogido mas o menos como se estructura la api. Este script intenta hacer un uso responsable de esta, ya que es una gran herramienta para estadísticos de andar por casa.
--- collector.py
+++ collector.py
@@ -1,59 +1,72 @@
 import requests, time, json
-from collections import OrderedDict
 
 
 URL = 'https://tienda.mercadona.es/api/categories/'
 
+# funcion recursiva que escaneara la api
 
-categories = requests.get(URL).json()['results']
+def fillProducts(url: str, content: list, products: list = [], root = 'categories', repeatIfDenied: int = 3):
+	'''url: enlace donde se encuentra el siguiente json a analizar
+	content: lista referenciada fuera de la funcion a rellenar con los datos de ese json
+	products: lista de productos vista en el anterior json, si no se encuentran mas json seran los productos definitivos
+	root: raiz del json donde buscar una lista de categorias
+	repeatIfDenied: si el servidor se quejara de que se estan lanzando demasiadas llamadas se reintenta este numero de veces
+	'''
+    
+	r = requests.get(url)
+	if r.status_code == 200: # estamos viendo CATEGORIA GENERAL, CATEGORIA ESPECIFICA o TIPO DE PRODUCTO
+		for item in r.json()[root]:
+			content.append(
+				{
+					'id': item['id'],
+					'name': item['name'],
+					'content': [],
+					}
+					)
+			time.sleep(2) # retraso para no sobrecargar el servidor
+			fillProducts(
+				url = URL + str(item['id']),
+				content = content[-1]['content'],
+				products = item.get('products', []),
+				)
+	elif r.status_code == 429: # Solicitud denegada por recibir demasiadas peticiones seguidas
+			time.sleep(60)
+			# se vuelve a intentar una vez mas
+			if repeatIfDenied:
+				fillProducts(
+						url = url,
+						content = content,
+						products = products,
+						repeatIfDenied = repeatIfDenied - 1
+						)
+			else:
+				raise Exception('Solicitud denegada por recibir demasiadas peticiones seguidas')
+								
+	elif r.status_code == 410: # ya no hay mas sublistas
+		for item in products:
+			pi = item['price_instructions']
+
+			product = {
+				'id': item['id'],
+				'name': item['display_name'],
+				'iva': pi['iva'],
+				'unit_size': pi['unit_size'],
+				'bulk_price': pi['bulk_price'],
+				'unit_price': pi['unit_price'],
+				'size_format': pi['size_format'],
+				}
+			
+			content.append(product)
+			time.sleep(1) # retraso para no sobrecargar el servidor
 
 
-data = {}
-for cat in categories:
-    # guardamos la categoría por su nombre, necesitamos el id para obtener la url
-    cat_name = cat['name']
-    cat_url = URL + str(cat['id'])
+# rellenar los datos
+products = []
+fillProducts(url=URL, content=products, root='results')
 
-    # dentro de cada categoria estan las subcategorias
-    for subcat in requests.get(cat_url).json()['categories']:
-        # dentro de la subcategoria los productos
-        subcat_name, subcat_products = subcat['name'], subcat['products']
-        # de cada producto nos quedaremos con:
-        # - id [id] -> se usará como índice, suponiendo que será mas estable que el nombre
-        # - nombre [display_name]
-        ## price_instructions
-            # - iva [iva]
-            # - tamaño [unit_size]
-            # - precio por kg/L... [bulk_price]
-            # - precio por unidad [unit_price]
-            # - unidades del formato [size_format]
-        for product in subcat_products:
-            product_id = product['id']
-            pi = product['price_instructions']
-            data[product_id] = {
-                'name': product['display_name'],
-                'iva': pi['iva'],
-                'unit_size': pi['unit_size'],
-                'bulk_price': pi['bulk_price'],
-                'unit_price': pi['unit_price'],
-                'size_format': pi['size_format'],
-                'category': cat_name,
-                'subcategory': subcat_name
-            }
-
-    time.sleep(0.5) # esperar un poco entre llamadas a la API
-
-
-
-# organizamos los datos por id, por tenerlos siempre de la misma manera
-dataSorted = OrderedDict(
-    sorted(data.items(), key=lambda k: float(k[0]))
-    )
-
-
-# guardamos el resultado en un json
+# grabarlos en un json, que se irá guardando en la historia de git
 with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(dataSorted, f,
+    json.dump(products, f,
               indent='\t' # + legible
               , ensure_ascii=False # para que aparezcan las tildes
-              )
+			  )
